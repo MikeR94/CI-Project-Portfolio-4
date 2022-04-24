ad7ece139dc016ee683f9d5e41c2d728e2df23e3
miker94 <michael_ralph@hotmail.com>
2022-04-24 18:22:40 +0100

    Install django-allauth and update settings.py accordingly

--- Cafe_Manbo/settings.py
+++ Cafe_Manbo/settings.py
@@ -48,8 +48,15 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'Home',
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
