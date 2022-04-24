4f7a8f46173af2916fb67587c2ff1842c6a9d8e2
miker94 <michael_ralph@hotmail.com>
2022-04-25 00:24:34 +0100

    Add javascript to change the colour of the navigation bar in regards to the current page offset

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,11 @@
 console.log("JavaScript has been loaded")
+
+let nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset > 950) {
+    nav.classList.add('bg-dark', 'shadow');
+  } else {
+    nav.classList.remove('bg-dark', 'shadow');
+  }
+});
