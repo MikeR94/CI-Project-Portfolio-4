a8d46db6e6e0d7cd673ae58c1e284fe0df53f160
miker94 <michael_ralph@hotmail.com>
2022-04-25 19:37:42 +0100

    Add JQeury CDN, add staff-all-booking.html and link up to datatables and design pending-bookings.html

--- Home/urls.py
+++ Home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="home"),
-    path("staff/dashboard", views.staff_dashboard, name="staff-dashboard")
+    path("staff/dashboard", views.staff_dashboard, name="staff-dashboard"),
+    path("staff/pending-bookings", views.staff_pending_bookings, name="staff-pending-bookings"),
 ]
--- Home/views.py
+++ Home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from Booking.models import Booking
 
 # Create your views here.
 
@@ -9,3 +10,11 @@ def staff_dashboard(request):
     if request.user.is_staff:
         return render(request, "staff/staff-dashboard.html")
     return render(request, "index.html")
+
+def staff_pending_bookings(request):
+    if request.user.is_staff:
+        pending_bookings = Booking.objects.filter(guest_attended=False)
+        context = {
+            "pending_bookings": pending_bookings,
+        }
+        return render(request, "staff/staff-pending-bookings.html", context)
