442d0ba9b5fafbacfaf1882d701ce2003fd3df3c
miker94 <michael_ralph@hotmail.com>
2022-04-25 23:22:06 +0100

    Add Chart.JS and implement functionality to display number of guests per month for the year 2022

--- Home/views.py
+++ Home/views.py
@@ -4,8 +4,37 @@ from django.http import HttpResponseRedirect
 from django.core.mail import EmailMessage
 from django.conf import settings
 from django.template.loader import render_to_string
+import datetime as dt
+from django.db.models import Sum
+from Home.utils import (
+    jan_start,
+    jan_end,
+    feb_start,
+    feb_end,
+    mar_start,
+    mar_end,
+    apr_start,
+    apr_end,
+    may_start,
+    may_end,
+    jun_start,
+    jun_end,
+    jul_start,
+    jul_end,
+    aug_start,
+    aug_end,
+    sep_start,
+    sep_end,
+    oct_start,
+    oct_end,
+    nov_start,
+    nov_end,
+    dec_start,
+    dec_end,
+)
 
 # Create your views here.
+
 
 def index(request):
     return render(request, "index.html")
@@ -13,7 +42,93 @@ def index(request):
 
 def staff_dashboard(request):
     if request.user.is_staff:
-        return render(request, "staff/staff-dashboard.html")
+        jan_count = (
+            Booking.objects.filter(guest_attended=True)
+            .filter(date_of_visit__gte=jan_start)
+            .filter(date_of_visit__lte=jan_end)
+            .aggregate(sum=Sum("number_of_guests"))["sum"]
+        )
+        feb_count = (
+            Booking.objects.filter(guest_attended=True)
+            .filter(date_of_visit__gte=feb_start)
+            .filter(date_of_visit__lte=feb_end)
+            .aggregate(sum=Sum("number_of_guests"))["sum"]
+        )
+        mar_count = (
+            Booking.objects.filter(guest_attended=True)
+            .filter(date_of_visit__gte=mar_start)
+            .filter(date_of_visit__lte=mar_end)
+            .aggregate(sum=Sum("number_of_guests"))["sum"]
+        )
+        apr_count = (
+            Booking.objects.filter(guest_attended=True)
+            .filter(date_of_visit__gte=apr_start)
+            .filter(date_of_visit__lte=apr_end)
+            .aggregate(sum=Sum("number_of_guests"))["sum"]
+        )
+        may_count = (
+            Booking.objects.filter(guest_attended=True)
+            .filter(date_of_visit__gte=may_start)
+            .filter(date_of_visit__lte=may_end)
+            .aggregate(sum=Sum("number_of_guests"))["sum"]
+        )
+        jun_count = (
+            Booking.objects.filter(guest_attended=True)
+            .filter(date_of_visit__gte=jun_start)
+            .filter(date_of_visit__lte=jun_end)
+            .aggregate(sum=Sum("number_of_guests"))["sum"]
+        )
+        jul_count = (
+            Booking.objects.filter(guest_attended=True)
+            .filter(date_of_visit__gte=jul_start)
+            .filter(date_of_visit__lte=jul_end)
+            .aggregate(sum=Sum("number_of_guests"))["sum"]
+        )
+        aug_count = (
+            Booking.objects.filter(guest_attended=True)
+            .filter(date_of_visit__gte=aug_start)
+            .filter(date_of_visit__lte=aug_end)
+            .aggregate(sum=Sum("number_of_guests"))["sum"]
+        )
+        sep_count = (
+            Booking.objects.filter(guest_attended=True)
+            .filter(date_of_visit__gte=sep_start)
+            .filter(date_of_visit__lte=sep_end)
+            .aggregate(sum=Sum("number_of_guests"))["sum"]
+        )
+        oct_count = (
+            Booking.objects.filter(guest_attended=True)
+            .filter(date_of_visit__gte=oct_start)
+            .filter(date_of_visit__lte=oct_end)
+            .aggregate(sum=Sum("number_of_guests"))["sum"]
+        )
+        nov_count = (
+            Booking.objects.filter(guest_attended=True)
+            .filter(date_of_visit__gte=nov_start)
+            .filter(date_of_visit__lte=nov_end)
+            .aggregate(sum=Sum("number_of_guests"))["sum"]
+        )
+        dec_count = (
+            Booking.objects.filter(guest_attended=True)
+            .filter(date_of_visit__gte=dec_start)
+            .filter(date_of_visit__lte=dec_end)
+            .aggregate(sum=Sum("number_of_guests"))["sum"]
+        )
+        context = {
+            "jan_count": jan_count,
+            "feb_count": feb_count,
+            "mar_count": mar_count,
+            "apr_count": apr_count,
+            "may_count": may_count,
+            "jun_count": jun_count,
+            "jul_count": jul_count,
+            "aug_count": aug_count,
+            "sep_count": sep_count,
+            "oct_count": oct_count,
+            "nov_count": nov_count,
+            "dec_count": dec_count,
+        }
+        return render(request, "staff/staff-dashboard.html", context)
     return render(request, "index.html")
 
 
@@ -29,12 +144,12 @@ def staff_pending_bookings(request):
 def staff_approve_booking(request, booking_id):
     next = request.POST.get("next", "/")
     booking = Booking.objects.filter(id=booking_id)
-    template = render_to_string('approved_email_template.html')
+    template = render_to_string("approved_email_template.html")
     email = EmailMessage(
-        'Cafe Manbo - [BOOKING APPROVED]',
+        "Cafe Manbo - [BOOKING APPROVED]",
         template,
         settings.EMAIL_HOST_USER,
-        ['mikeyralph@hotmail.co.uk'],
+        ["mikeyralph@hotmail.co.uk"],
     )
     email.fail_silently = False
     email.send()
@@ -48,12 +163,12 @@ def staff_approve_booking(request, booking_id):
 def staff_deny_booking(request, booking_id):
     next = request.POST.get("next", "/")
     booking = Booking.objects.filter(id=booking_id)
-    template = render_to_string('denied_email_template.html')
+    template = render_to_string("denied_email_template.html")
     email = EmailMessage(
-        'Cafe Manbo - [BOOKING DENIED]',
+        "Cafe Manbo - [BOOKING DENIED]",
         template,
         settings.EMAIL_HOST_USER,
-        ['mikeyralph@hotmail.co.uk'],
+        ["mikeyralph@hotmail.co.uk"],
     )
     email.fail_silently = False
     email.send()
