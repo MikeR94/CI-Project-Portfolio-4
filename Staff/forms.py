90f435a644df0d3a9dbcca809dadb0b62502c458
miker94 <michael_ralph@hotmail.com>
2022-04-29 11:30:04 +0100

    Add date_of_visit widget to EditBookingForm

--- Staff/forms.py
+++ Staff/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from Booking.models import Booking
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class EditBookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['first_name', 'last_name', 'contact_number', "time_of_visit", "date_of_visit", "number_of_guests", "email"]
+        widgets = {
+            'date_of_visit': DateInput(),
+        }
