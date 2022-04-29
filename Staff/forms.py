7f4080a53b2ae5d02f5cfa21372c891e9ddbb4d4
miker94 <michael_ralph@hotmail.com>
2022-04-29 11:27:42 +0100

    Remove checkbox fields from EditBookingForm

--- Staff/forms.py
+++ Staff/forms.py
@@ -4,4 +4,4 @@ from Booking.models import Booking
 class EditBookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['first_name', 'last_name', 'contact_number', "time_of_visit", "date_of_visit", "number_of_guests", "guest_attended", "bill_settled", "booking_approved", "booking_denied", "booking_acknowledged", "email"]
+        fields = ['first_name', 'last_name', 'contact_number', "time_of_visit", "date_of_visit", "number_of_guests", "email"]
