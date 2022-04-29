595d5da6180914c22fa70aab8035880b60869d93
miker94 <michael_ralph@hotmail.com>
2022-04-29 14:08:24 +0100

    Create PaymentForm in Staff forms.py file

--- Staff/forms.py
+++ Staff/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from Booking.models import Booking
+from Staff.models import Payment
 
 class DateInput(forms.DateInput):
     input_type = 'date'
@@ -11,3 +12,12 @@ class EditBookingForm(forms.ModelForm):
         widgets = {
             'date_of_visit': DateInput(),
         }
+
+class PaymentForm(forms.ModelForm):
+    class Meta:
+        model = Payment
+        fields = ['amount_owed', 'amount_paid', 'amount_tipped', 'total_income']
+        widgets = {
+            'total_income': forms.HiddenInput(),
+            'amount_tipped': forms.HiddenInput(),
+            }
