4538ea0f2dbd9fc24cdbda4bb61d43d419af21b5
miker94 <michael_ralph@hotmail.com>
2022-05-03 18:38:49 +0100

    Add functionality to allow the user to load details about their bookings - WIP

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("user-reservations", views.show_user_reservations, name="user_reservations"),
+    path("user-edit-booking/<booking_id>", views.user_edit_booking, name="user_edit_booking"),
 ]
--- Accounts/views.py
+++ Accounts/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from Booking.models import Booking
 
 # Create your views here.
 
 def show_user_reservations(request):
-    test = Booking.objects.filter(user=request.user)
+    booking = Booking.objects.filter(user=request.user)
     context = {
-        "test": test
+        "booking": booking
     }
     return render(request, "user_reservations.html", context)
+
+
+def user_edit_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    context = {
+        "booking": booking,
+    }
+    return render(request, "user_edit_booking.html", context)
