5aa6458cd45d8fb05f4fc164886dc42cf137f417
miker94 <michael_ralph@hotmail.com>
2022-05-04 12:38:25 +0100

    Add functionality to disable users from editing bookings when the booking has been completed and the guest has arrived

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from Booking.models import Booking
 from django.http import HttpResponseRedirect
 from Staff.forms import EditBookingForm
