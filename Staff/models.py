4237e194050681be1d79b6af4aaed2ded4351608
miker94 <michael_ralph@hotmail.com>
2022-05-04 15:14:10 +0100

    Revert back to original Payment model

--- Staff/models.py
+++ Staff/models.py
@@ -4,8 +4,8 @@ from Booking.models import Booking
 # Create your models here.
 
 class Payment(models.Model):
-    amount_owed = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
-    amount_paid = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
+    amount_owed = models.DecimalField(max_digits=6, decimal_places=2)
+    amount_paid = models.DecimalField(max_digits=6, decimal_places=2)
     amount_tipped = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
     total_income = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=True)
     booking = models.ForeignKey(Booking, null=True, on_delete=models.SET_NULL)
