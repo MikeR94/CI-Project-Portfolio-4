97fc17a8f01ba2f30bf5cb2977df872897442e89
miker94 <michael_ralph@hotmail.com>
2022-05-04 15:09:57 +0100

    Add blank=True to payment model amount owed and amount paid fields and also disable edit booking if the guest has attended

--- Staff/models.py
+++ Staff/models.py
@@ -4,10 +4,10 @@ from Booking.models import Booking
 # Create your models here.
 
 class Payment(models.Model):
-    amount_owed = models.DecimalField(max_digits=6, decimal_places=2)
-    amount_paid = models.DecimalField(max_digits=6, decimal_places=2)
+    amount_owed = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
+    amount_paid = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
     amount_tipped = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
-    total_income = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=True)
+    total_income = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     booking = models.ForeignKey(Booking, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
