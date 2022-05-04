ad4b0866f1241be8f47ef86787a3a32e6438be37
miker94 <michael_ralph@hotmail.com>
2022-05-04 15:13:07 +0100

    Fix mistake changing total_income field on Payment model to null=True. Revert back to null=False

--- Staff/models.py
+++ Staff/models.py
@@ -7,7 +7,7 @@ class Payment(models.Model):
     amount_owed = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
     amount_paid = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
     amount_tipped = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
-    total_income = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    total_income = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=True)
     booking = models.ForeignKey(Booking, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
