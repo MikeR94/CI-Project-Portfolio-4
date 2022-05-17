d5ea12a26306b5a0d7dfc49ad0491812ac8308ea
miker94 <michael_ralph@hotmail.com>
2022-05-17 19:29:11 +0100

    Add testcase to test if a user can visit their reservations when logged in

--- Accounts/tests.py
+++ Accounts/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from Accounts.views import show_user_reservations
+from Accounts.models import User
+from django.test import Client
 
-# Create your tests here.
+class TestUrls(TestCase):
+
+    def test_user_reservations_url_is_resolved(self):
+        self.user = User.objects.create_superuser(
+            username="admin",
+            password="adminadmin",
+            email="admin@example.com")
+        self.client.force_login(self.user)
+        response = self.client.get('/user-reservations')
+        self.assertEqual(response.status_code, 200)
+
+        url = reverse('user_reservations')
+        self.assertEquals(resolve(url).func, show_user_reservations)
+
