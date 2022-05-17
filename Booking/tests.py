17a8ac3338247cf46f104a1636e09437e0be69f7
miker94 <michael_ralph@hotmail.com>
2022-05-17 12:40:35 +0100

    Setup a mock database when in development mode to allow for testing and also perform 2 basic tests to check everthing is working

--- Booking/tests.py
+++ Booking/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from Booking.views import book_now
+from Home.views import index
 
-# Create your tests here.
+class TestUrls(TestCase):
+
+    def test_home_url_is_resolved(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+        url = reverse('home')
+        self.assertEquals(resolve(url).func, index)
+
+
+    def test_book_now_is_resolved(self):
+        response = self.client.get('/book-now')
+        self.assertEqual(response.status_code, 200)
+
+        url = reverse('book_now')
+        self.assertEquals(resolve(url).func, book_now)
