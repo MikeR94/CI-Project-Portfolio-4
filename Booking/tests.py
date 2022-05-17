658e2bebb64eea30a4f58196ea6755a3252100e8
miker94 <michael_ralph@hotmail.com>
2022-05-17 12:42:46 +0100

    Move test_home_is_resolved test into Home tests.py file

--- Booking/tests.py
+++ Booking/tests.py
@@ -1,16 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from Booking.views import book_now
-from Home.views import index
 
 class TestUrls(TestCase):
-
-    def test_home_url_is_resolved(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-
-        url = reverse('home')
-        self.assertEquals(resolve(url).func, index)
 
 
     def test_book_now_is_resolved(self):
--- Home/tests.py
+++ Home/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from Home.views import index
 
-# Create your tests here.
+class TestUrls(TestCase):
+
+    def test_home_url_is_resolved(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+        url = reverse('home')
+        self.assertEquals(resolve(url).func, index)
