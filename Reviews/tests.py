5f615002194c730da7391e9fa41b31a1040ab74c
miker94 <michael_ralph@hotmail.com>
2022-05-18 10:03:23 +0100

    Test create review url asserts a 200 status if the user is logged in

--- Reviews/tests.py
+++ Reviews/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from Accounts.models import User
+from Reviews.views import create_review
 
-# Create your tests here.
+class TestUrls(TestCase):
+
+    def test_create_review_url_is_resolved(self):
+        self.user = User.objects.create_user(
+            username="admin",
+            password="adminadmin",
+            email="admin@example.com")
+        self.client.force_login(self.user)
+        response = self.client.get('/create/review')
+        self.assertEqual(response.status_code, 200)
+
+        url = reverse('create_review')
+        self.assertEquals(resolve(url).func, create_review)
