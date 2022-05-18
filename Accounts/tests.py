57e21886d1ec2493feb4473f6a2e2527596f5df4
miker94 <michael_ralph@hotmail.com>
2022-05-18 09:27:36 +0100

    Add tests to test both edit and cancel user_details_booking

--- Accounts/tests.py
+++ Accounts/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from Accounts.views import show_user_reservations, user_details_booking
+from Accounts.views import show_user_reservations, user_cancel_booking, user_details_booking, user_edit_booking
 from Accounts.models import User
 
 from Booking.models import Booking
@@ -27,7 +27,7 @@ class TestUrls(TestCase):
             email="admin@example.com")
         self.client.force_login(self.user)
         Booking.objects.create(
-            id = "1",
+            id = "50",
             first_name = "Mike",
             last_name = "Ralph",
             email = "mikeyralph@hotmail.co.uk",
@@ -37,9 +37,57 @@ class TestUrls(TestCase):
             number_of_guests = "4",
             contact_number = "07946472421"
         )
-        response = self.client.get('/user-details-booking/1')
+        response = self.client.get('/user-details-booking/50')
         self.assertEqual(response.status_code, 200)
 
         url = reverse('user_details_booking', kwargs={'booking_id': '50'})
         self.assertEquals(resolve(url).func, user_details_booking)
+
+
+    def test_user_details_booking_edit_url_is_resolved(self):
+        self.user = User.objects.create_user(
+            username="admin",
+            password="adminadmin",
+            email="admin@example.com")
+        self.client.force_login(self.user)
+        Booking.objects.create(
+            id = "50",
+            first_name = "Mike",
+            last_name = "Ralph",
+            email = "mikeyralph@hotmail.co.uk",
+            ref_number = "1234567890",
+            date_of_visit = "2022-05-16",
+            time_of_visit = "20:00:00",
+            number_of_guests = "4",
+            contact_number = "07946472421"
+        )
+        response = self.client.get('/user-details-booking/edit/50')
+        self.assertEqual(response.status_code, 200)
+
+        url = reverse('edit', kwargs={'booking_id': '50'})
+        self.assertEquals(resolve(url).func, user_edit_booking)
+
+
+    def test_user_details_booking_cancel_url_is_resolved(self):
+        self.user = User.objects.create_user(
+            username="admin",
+            password="adminadmin",
+            email="admin@example.com")
+        self.client.force_login(self.user)
+        Booking.objects.create(
+            id = "50",
+            first_name = "Mike",
+            last_name = "Ralph",
+            email = "mikeyralph@hotmail.co.uk",
+            ref_number = "1234567890",
+            date_of_visit = "2022-05-16",
+            time_of_visit = "20:00:00",
+            number_of_guests = "4",
+            contact_number = "07946472421"
+        )
+        response = self.client.get('/user-details-booking/cancel/50')
+        self.assertEqual(response.status_code, 302)
+
+        url = reverse('cancel', kwargs={'booking_id': '50'})
+        self.assertEquals(resolve(url).func, user_cancel_booking)
 
