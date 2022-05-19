ba7b815c588ac2fbcd78a2574f87d1d4f64bbc1b
miker94 <michael_ralph@hotmail.com>
2022-05-19 11:31:01 +0100

    Change address on website to a fake adress

--- static/js/map.js
+++ static/js/map.js
@@ -1,6 +1,6 @@
 function initMap() {
   // The location of Uluru
-  const uluru = { lat: 53.681, lng: -1.578 };
+  const uluru = { lat: 53.683, lng: -1.4992 };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 16,
