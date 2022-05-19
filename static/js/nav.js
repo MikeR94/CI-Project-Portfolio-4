4a50a23306cd9ea6cb61f6dac9c6aa06f65a3412
miker94 <michael_ralph@hotmail.com>
2022-05-19 21:11:24 +0100

    Fix navigation bar styling issue causing the transparent bar to have the dark background

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,9 +1,17 @@
 let nav = document.querySelector('nav');
+let bgColor = document.getElementsByClassName('dropdown-menu')
 
 window.addEventListener('scroll', function () {
   if (window.pageYOffset > 350) {
     nav.classList.add('bg-dark', 'shadow');
+    for (let i = 0; i < bgColor.length; i++) {
+      bgColor[i].style.backgroundColor = '#212529';
+    }
+
   } else {
     nav.classList.remove('bg-dark', 'shadow');
+    for (let i = 0; i < bgColor.length; i++) {
+      bgColor[i].style.backgroundColor = '#21252900';
+    }
   }
 });
