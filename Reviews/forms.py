1d68c4ef70ab9ab3a853d573bb2345296cbbd1ce
miker94 <michael_ralph@hotmail.com>
2022-05-21 00:04:28 +0100

    Add form validation to only allow characters for first and last name on ReviewForm

--- Reviews/forms.py
+++ Reviews/forms.py
@@ -5,3 +5,7 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ['first_name', 'last_name', 'body']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control', 'pattern':'[A-Za-z ]+', 'title':'Please enter characters only'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control', 'pattern':'[A-Za-z ]+', 'title':'Please enter characters only'}),
+        }
