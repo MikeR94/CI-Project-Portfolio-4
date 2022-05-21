1d3a3942c9967e6cdf24ce3fd5972d687d83ee5e
miker94 <michael_ralph@hotmail.com>
2022-05-21 18:45:41 +0100

    Remove ReviewForm function to check for spaces

--- Reviews/forms.py
+++ Reviews/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from Reviews.models import Review
-import re
 
 class ReviewForm(forms.ModelForm):
     class Meta:
@@ -10,9 +9,3 @@ class ReviewForm(forms.ModelForm):
             'first_name': forms.TextInput(attrs={'class': 'form-control', 'pattern':'[A-Za-z ]+', 'title':'Please enter characters only'}),
             'last_name': forms.TextInput(attrs={'class': 'form-control', 'pattern':'[A-Za-z ]+', 'title':'Please enter characters only'}),
         }
-
-    def clean_body(self):
-        body = self.cleaned_data['body']
-        if re.match('/[\s]+$',body):
-            raise forms.ValidationError("Spaces not allowed")
-        return body
