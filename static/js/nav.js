7de698a19b55e12a1b8f8db8edd912de758fc93b
miker94 <michael_ralph@hotmail.com>
2022-05-23 11:48:03 +0100

    Improve navigation bar colour responsiveness

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,19 +1,6 @@
 let nav = document.querySelector('nav');
 let bgColor = document.getElementsByClassName('dropdown-menu')
 
-window.addEventListener('resize', function() {
-  if (screen.width < 1200) {
-    nav.classList.add('bg-dark', 'shadow');
-    for (let i = 0; i < bgColor.length; i++) {
-      bgColor[i].style.backgroundColor = '#212529';
-    }
-  } else {
-    nav.classList.remove('bg-dark', 'shadow');
-    for (let i = 0; i < bgColor.length; i++) {
-      bgColor[i].style.backgroundColor = '#21252900';
-    }
-  }
-});
 
 window.addEventListener('scroll', function () {
   if (window.pageYOffset > 350) {
@@ -21,7 +8,6 @@ window.addEventListener('scroll', function () {
     for (let i = 0; i < bgColor.length; i++) {
       bgColor[i].style.backgroundColor = '#212529';
     }
-
   } else {
     nav.classList.remove('bg-dark', 'shadow');
     for (let i = 0; i < bgColor.length; i++) {
