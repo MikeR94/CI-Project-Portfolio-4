652256dc05e3d2ba59c2cfedd8f672018285ec04
miker94 <michael_ralph@hotmail.com>
2022-05-23 12:45:58 +0100

    Add debug=True to live website since receiving 500 server errors from latest update

--- Cafe_Manbo/settings.py
+++ Cafe_Manbo/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ["cafe-manbo.herokuapp.com", "127.0.0.1", "localhost"]
 
