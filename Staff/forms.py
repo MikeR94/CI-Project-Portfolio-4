981fcdd24ee7ccb4b8399f7f743427402921dfed
miker94 <michael_ralph@hotmail.com>
2022-05-23 14:30:39 +0100

    Remove commented out code in staff forms.py

--- Staff/forms.py
+++ Staff/forms.py
@@ -1,24 +1,6 @@
 from django import forms
-from Booking.models import Booking
 from Staff.models import Payment
 
-# class TimeInput(forms.TimeInput):
-#     input_type = 'time'
-
-# class DateInput(forms.DateInput):
-#     input_type = 'date'
-
-
-# class EditBookingForm(forms.ModelForm):
-#     class Meta:
-#         model = Booking
-#         fields = ['first_name', 'last_name', 'contact_number', "time_of_visit", "date_of_visit", "number_of_guests", "email"]
-#         widgets = {
-#             'date_of_visit': DateInput(),
-#             'first_name': forms.TextInput(attrs={'class': 'form-control', 'pattern':'[A-Za-z ]+', 'title':'Please enter characters only'}),
-#             'last_name': forms.TextInput(attrs={'class': 'form-control', 'pattern':'[A-Za-z ]+', 'title':'Please enter characters only'}),
-#             'time_of_visit': forms.Select(attrs={'class': 'form-control'}),
-#         }
 
 class PaymentForm(forms.ModelForm):
     class Meta:
