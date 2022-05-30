5acb7a8ab3dd705069ea0dfa289d8a2be1b79744
miker94 <michael_ralph@hotmail.com>
2022-05-30 09:20:01 +0100

    Fix code that was checking for sunday dates. Created an array with all sundays and now looping through them to check if the date_of_visit == one of the pre determined sunday dates and if so, throw a form validation error

--- Booking/forms.py
+++ Booking/forms.py
@@ -1,54 +1,77 @@
 from django import forms
 from Booking.models import Booking
 from datetime import date
-import calendar
 from django.utils import timezone
+from Booking.utils import sunday
+
 
 class DateInput(forms.DateInput):
-    input_type = 'date'
+    input_type = "date"
+
 
 class TimeInput(forms.TimeInput):
-    input_type = 'time'
+    input_type = "time"
+
 
 class BookingForm(forms.ModelForm):
-
     class Meta:
         model = Booking
-        fields = ["first_name", "last_name", "email", "time_of_visit", "date_of_visit", "number_of_guests", "contact_number", "additional_info", "disabled_access"]
+        fields = [
+            "first_name",
+            "last_name",
+            "email",
+            "time_of_visit",
+            "date_of_visit",
+            "number_of_guests",
+            "contact_number",
+            "additional_info",
+            "disabled_access",
+        ]
         widgets = {
-            'date_of_visit': DateInput(),
-            'first_name': forms.TextInput(attrs={'class': 'form-control', 'pattern':'[A-Za-z ]+', 'title':'Please enter characters only'}),
-            'last_name': forms.TextInput(attrs={'class': 'form-control', 'pattern':'[A-Za-z ]+', 'title':'Please enter characters only'}),
-            'time_of_visit': forms.Select(attrs={'class': 'form-control'}),
-            'contact_number': forms.TextInput(attrs={'class': 'form-control', 'type':'number'})
+            "date_of_visit": DateInput(),
+            "first_name": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "pattern": "[A-Za-z ]+",
+                    "title": "Please enter characters only",
+                }
+            ),
+            "last_name": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "pattern": "[A-Za-z ]+",
+                    "title": "Please enter characters only",
+                }
+            ),
+            "time_of_visit": forms.Select(attrs={"class": "form-control"}),
+            "contact_number": forms.TextInput(
+                attrs={"class": "form-control", "type": "number"}
+            ),
         }
 
     def clean(self):
         cleaned_data = super(BookingForm, self).clean()
-        time_of_visit = cleaned_data.get('time_of_visit')
-        date_of_visit = cleaned_data.get('date_of_visit')
+        time_of_visit = cleaned_data.get("time_of_visit")
+        date_of_visit = cleaned_data.get("date_of_visit")
         today = date.today()
         time = timezone.now().time()
 
-        year = 2022
-        A=calendar.TextCalendar(calendar.SUNDAY)
-        for month in range(1,13):
-            for day in A.itermonthdays(year, month):
-                if day != 0:
-                    days = date(year, month ,day)
-                    if days.weekday()==6:
-                        sunday1 = (f'{year}-0{month}-{day}')
-                        sunday2 = (f'{year}-{month}-{day}')
-                        if str(date_of_visit) == str(sunday1):
-                            self._errors['date_of_visit'] = self.error_class(['Sorry we are closed on sundays'])
-                            del self.cleaned_data['date_of_visit']
-                        if str(date_of_visit) == str(sunday2):
-                            self._errors['date_of_visit'] = self.error_class(['Sorry we are closed on sundays'])
-                            del self.cleaned_data['date_of_visit']
+        for x in sunday:
+            if x == str(date_of_visit):
+                self._errors["date_of_visit"] = self.error_class(
+                    ["Sorry we are only taking bookings for this year"]
+                )
+                del self.cleaned_data["date_of_visit"]
+
+        if "2023" in str(date_of_visit):
+            self._errors["date_of_visit"] = self.error_class(
+                ["Sorry we are only taking bookings for this year"]
+            )
+            del self.cleaned_data["date_of_visit"]
 
         if date_of_visit == today and time_of_visit < time:
-            self._errors['time_of_visit'] = self.error_class(['You cant book in the past'])
-            del self.cleaned_data['time_of_visit']
+            self._errors["time_of_visit"] = self.error_class(
+                ["You cant book in the past"]
+            )
+            del self.cleaned_data["time_of_visit"]
         return cleaned_data
-        
-    
