2d994e39a67809575eb3bd210f668703d344938c
miker94 <michael_ralph@hotmail.com>
2022-06-07 14:12:17 +0100

    Change to outlook to provide the email service as google was throwing smtp authetication errors often

--- Cafe_Manbo/settings.py
+++ Cafe_Manbo/settings.py
@@ -177,7 +177,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 #SMTP Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp-mail.outlook.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
