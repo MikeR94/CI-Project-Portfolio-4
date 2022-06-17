ad068ae0be82f447abdf7e1af7582f93eb3f6a3d
miker94 <michael_ralph@hotmail.com>
2022-06-17 11:14:05 +0100

    Update test_booking_for_date_of_visit_error is sending the correct validation message

--- booking/tests.py
+++ booking/tests.py
@@ -134,5 +134,5 @@ class TestForms(TestCase):
         test_form = BookingForm(request.POST)
         self.assertFalse(test_form.is_valid())
         self.assertEqual(
-            test_form.errors["date_of_visit"][0], "You can't book in the past"
+            test_form.errors["date_of_visit"][0], "Sorry we are only taking bookings for this year"
         )
